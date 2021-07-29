2ec4261bba44cc22e4a2daec0b7052543c0d16cd
hayabusa333 <junnichi0121@gmail.com>
2021-07-29 10:19:50 +0900

    死活監視のルーティングを追加

--- web-api/src/main.go
+++ web-api/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 	"github.com/gorilla/mux"
 )
 
@@ -11,10 +12,18 @@ func main() {
 	// 単純なハンドラ
 	r.HandleFunc("/", EchoHandler)
 
+        // 死活監視
+        r.HandleFunc("/heartbeat", HearthbeatHandler)
+
 	// サービスを起動する
 	http.ListenAndServe(":8080", r)
 }
 
 func EchoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Gorilla!\n"))
+}
+
+func HearthbeatHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "OK")
 }
